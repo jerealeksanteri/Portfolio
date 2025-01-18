eea6910756637121ba506fb78538acc6c6b41486
SolteqJere <jere.niemi@solteq.com>
2025-01-18 17:04:01 +0200

    nav bar, animation changes in terminal, fixed scrolling, fixed mobile view

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-const terminalText = ["> Hello stranger", "> Yes YOU!", "> My name is Jere Niemi", "> Welcome to my profile", "> . . . . . . . :)"];
+const terminalText = [
+    "> Hello stranger", 
+    "> Yes YOU!", 
+    "> My name is Jere Niemi", 
+    "> Welcome to my profile", 
+    "> . . . . . . . :)"
+];
 const terminalDisplay = document.getElementById("terminal-text");
+const cursor = document.getElementById("cursor");
 
 let index = 0;
 let charIndex = 0;
@@ -9,21 +16,37 @@ const lineSpeed = 700;
 function type() {
     if (index < terminalText.length) {
         if (charIndex < terminalText[index].length) {
-            terminalDisplay.textContent += terminalText[index][charIndex];
+            // Get the text content
+            const text = terminalText[index].substring(0, charIndex + 1);
+            // Replace the text content with the new text content
+            terminalDisplay.innerHTML = text.replace(/\n/g, "<br>");
             charIndex++;
             setTimeout(type, typingSpeed);
         } else {
-            terminalDisplay.textContent += "\n";
+            terminalDisplay.innerHTML += "<br>";
             index++;
             charIndex = 0;
             setTimeout(type, lineSpeed);
         }
     } else if (index === terminalText.length) {
+        cursor.style.display = "none";
         scrollToSection(1);
     }
 }
 
-window.onload = type;
+// Move the cursor
+function updateCursorPosition() {
+    // Get the text content
+    const text = terminalDisplay.textContent;
+
+    // Add the cursor
+    terminalDisplay.appendChild(cursor);
+}
+
+window.onload = () => {
+    setInterval(updateCursorPosition, 50); // Regularly update the cursor position
+    type();
+};
 
 // Scroll behavior to ensure one section scroll per action
 const sections = document.querySelectorAll('section');
@@ -51,4 +74,32 @@ function handleScroll(event) {
     }, 1200);
 }
 
+
+// For mobile
+let touchStartY = 0;
+let touchEndY = 0;
+
+function handleTouchStart(event) {
+    touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchEnd(event) {
+    touchEndY = event.changedTouches[0].clientY;
+    const direction = touchStartY - touchEndY > 0 ? 1 : -1;
+
+    if (!isScrolling) {
+        isScrolling = true;
+        const nextSectionIndex = currentSectionIndex + direction;
+
+        scrollToSection(nextSectionIndex);
+
+        setTimeout(() => {
+            isScrolling = false;
+        }, 1200); 
+    }
+}
+
+
 window.addEventListener('wheel', handleScroll, { passive: false });
+window.addEventListener('touchstart', handleTouchStart, { passive: false });
+window.addEventListener('touchend', handleTouchEnd, { passive: false });
